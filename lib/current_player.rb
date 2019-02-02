6627df4731e94e2da7ee55fa4da4bb5f0df85f58
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 08:24:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
-def turn_count
-  turn_count.each do |count|
-    puts count.to_i
+require_relative '../spec/current_player_spec.rb'
+def turn_count(board)
+count = 0
+for i in board 
+  if i == "X" || i == "O"
+    count += 1
   end
 end
+  
+turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
