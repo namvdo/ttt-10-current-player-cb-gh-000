3899435a76aac33e402bc56f3d3340310357bf5d
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 09:02:41 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+require_relative '../spec/current_player_spec.rb'
+def turn_count(board)
+counter = 0
+for i in board
+  if i == "X" || i == "O"
+    counter += 1
+  end
+end
+  return counter
+end
+def current_player(board)
+  current_move = turn_count(board)
+  if current_move % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
