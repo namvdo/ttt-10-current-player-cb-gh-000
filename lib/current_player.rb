38898abb5babb60bcb031c1f921e60c23018ec57
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 08:24:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,11 @@
 require_relative '../spec/current_player_spec.rb'
 def turn_count(board)
 count = 0
-for i in board
+for i in board 
   if i == "X" || i == "O"
     count += 1
   end
 end
-
+end
+  
 turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
